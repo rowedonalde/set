16b980a3a6bb439379640a80b4ee760530b6825a
Don Rowe <rowedonalde@gmail.com>
2016-10-09 20:41:16 -0700

    Add check for whether a hand is a Set

--- setgame/hand.py
+++ setgame/hand.py
@@ -2,3 +2,32 @@ class SetHand(object):
 
     def __init__(self, card1, card2, card3):
         self.cards = set([card1, card2, card3])
+
+    def is_set(self):
+        '''
+        Return true if these three cards represent a valid set in
+        the card game Set.
+
+        The three cards represent a valid set if, for each of the
+        four attributes, the cards are all the same or all different
+        for that attribute.'''
+
+        # A dict of the number of occurrences of each attribute
+        # value. If the cards are a valid set, each attribute may
+        # be seen 0 times (if that attribute were the same across the
+        # cards, but for another value), 1 time (if the cards were
+        # all different for that attribute), or 3 times (if the cards
+        # all were the same for that attribute, and it was that value).
+        # No attribute value can be seen more than 3 times in a hand,
+        # therefore, we know that this hand is not a valid set if
+        # any value is seen exactly 2 times.
+        attribute_occurrences = dict()
+
+        for card in self.cards:
+            for attr in (card.count, card.color, card.shading, card.shape):
+                try:
+                    attribute_occurrences[attr] += 1
+                except KeyError:
+                    attribute_occurrences[attr] = 1
+
+        return 2 not in attribute_occurrences.values()
--- test/test_set_hand.py
+++ test/test_set_hand.py
@@ -32,3 +32,81 @@ class TestSetGameHand(unittest.TestCase):
 
         for card in (card1, card2, card3):
             self.assertIn(card, hand.cards)
+
+    def test_hand_is_set_when_all_attributes_different(self):
+        card1 = SetCard(
+            count=1,
+            color=SetColor.red,
+            shading=SetShading.solid,
+            shape=SetShape.squiggles
+        )
+
+        card2 = SetCard(
+            count=2,
+            color=SetColor.green,
+            shading=SetShading.empty,
+            shape=SetShape.diamond
+        )
+
+        card3 = SetCard(
+            count=3,
+            color=SetColor.purple,
+            shading=SetShading.striped,
+            shape=SetShape.oval
+        )
+
+        hand = SetHand(card1, card2, card3)
+
+        self.assertTrue(hand.is_set())
+
+    def test_hand_is_set_with_mixture_of_all_different_and_all_same(self):
+        card1 = SetCard(
+            count=1,
+            color=SetColor.red,
+            shading=SetShading.solid,
+            shape=SetShape.squiggles
+        )
+
+        card2 = SetCard(
+            count=1,
+            color=SetColor.green,
+            shading=SetShading.empty,
+            shape=SetShape.diamond
+        )
+
+        card3 = SetCard(
+            count=1,
+            color=SetColor.purple,
+            shading=SetShading.striped,
+            shape=SetShape.oval
+        )
+
+        hand = SetHand(card1, card2, card3)
+
+        self.assertTrue(hand.is_set())
+
+    def test_hand_is_not_set_when_some_attribute_is_neither_all_different_or_all_same(self):
+        card1 = SetCard(
+            count=1,
+            color=SetColor.red,
+            shading=SetShading.solid,
+            shape=SetShape.squiggles
+        )
+
+        card2 = SetCard(
+            count=1,
+            color=SetColor.green,
+            shading=SetShading.empty,
+            shape=SetShape.diamond
+        )
+
+        card3 = SetCard(
+            count=3,
+            color=SetColor.purple,
+            shading=SetShading.striped,
+            shape=SetShape.oval
+        )
+
+        hand = SetHand(card1, card2, card3)
+
+        self.assertFalse(hand.is_set())
