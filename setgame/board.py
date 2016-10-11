3e86895a2705166a7560f7bdeb13579311e5bec1
Don Rowe <don.rowe@us.imshealth.com>
2016-10-11 15:46:15 -0700

    Add waitlist functionality. find_set returns hand

--- setgame/board.py
+++ setgame/board.py
@@ -1,3 +1,5 @@
+from hand import SetHand
+
 class SetBoard(object):
     start_size = 12
     deal = 3
@@ -8,6 +10,10 @@ class SetBoard(object):
 
         self.cards_by_encoding = {}
         self.graveyard_by_encoding = {}
+
+        # Key: encoding
+        # Value: list of Python sets of two other encodings
+        self.waitlist = {}
 
     @property
     def cards(self):
@@ -56,28 +62,45 @@ class SetBoard(object):
         self.graveyard_by_encoding.update(tx)
         return tx
 
+    def add_to_waitlist(self, missing, partner_1, partner_2):
+        '''
+        Add the encoding of the missing card to a waitlist
+        associating that encoding with the encodings of
+        existing cards that would form a valid set with it.
+        Note that one card can match with more than one pair
+        of cards. This method trusts that the missing encoding
+        was calculated accurately by missing_encoding_from.'''
+
+        try:
+            self.waitlist[missing].append(set([partner_1, partner_2]))
+        except KeyError:
+            self.waitlist[missing] = [set([partner_1, partner_2])]
+
     def find_set(self):
         '''
-        Return a valid set of three cards from the board or
+        Return a valid hand of three cards from the board or
         None if there are no valid sets on the board.'''
-
-        return None
 
         # Search greedily for valid sets, and only compare
         # forward against cards we haven't tried so we don't
         # duplicate work:
-        #encodings = self.cards_by_encoding.keys()
+        encodings = self.cards_by_encoding.keys()
 
-        #for i in range(len(encodings)):
-        #    encoding_1 = encodings[i]
-        #    for encoding_2 in encodings[i+1:]:
-        #        encoding_3 = SetBoard.missing_encoding_from(
-        #            encoding_1,
-        #            encoding_2,
-        #        )
+        for i in range(len(encodings)):
+            encoding_1 = encodings[i]
+            for encoding_2 in encodings[i+1:]:
+                encoding_3 = SetBoard.missing_encoding_from(
+                    encoding_1,
+                    encoding_2
+                )
 
-        #        try:
-        #            return self.remove_set(encoding_3, encoding_2, encoding_1)
+                try:
+                    s = self.remove_set(encoding_3, encoding_2, encoding_1)
+                    return SetHand(s[encoding_1], s[encoding_2], s[encoding_3])
+                except KeyError:
+                    self.add_to_waitlist(encoding_3, encoding_1, encoding_2)
+
+        return None
 
     @staticmethod
     def missing_encoding_from(card_encoding_1, card_encoding_2):
--- test/test_set_board.py
+++ test/test_set_board.py
@@ -10,7 +10,7 @@ class TestSetGameBoard(unittest.TestCase):
 
         # "System Under Test," i.e., the board:
         self.SUT = SetBoard(self.deck)
- 
+
         self.card1 = SetCard(
             count=1,
             color=SetColor.red,
@@ -88,22 +88,211 @@ class TestSetGameBoard(unittest.TestCase):
         self.assertEqual(2, len(self.SUT.cards))
         self.assertEqual(0, len(self.SUT.graveyard))
 
-    def test_board_find_set_returns_None_if_no_set(self):
+    def test_board_find_set_returns_None_if_no_set_and_adds_to_waitlist(self):
+        encoding1 = self.card1.encoding
+        encoding2 = self.card2.encoding
         card3 = SetCard(
             count=3,
             color=SetColor.purple,
             shading=SetShading.solid,
             shape=SetShape.oval
         )
+        encoding3 = card3.encoding
 
         # These do not make a set:
         hand = SetHand(self.card1, self.card2, card3)
         self.assertFalse(hand.is_set())
 
         self.SUT.cards_by_encoding = {
-            self.card1.encoding: self.card1,
-            self.card2.encoding: self.card2,
-            card3.encoding: card3
+            encoding1: self.card1,
+            encoding2: self.card2,
+            encoding3: card3
         }
 
         self.assertIsNone(self.SUT.find_set())
+
+        missing_encoding = SetBoard.missing_encoding_from(encoding1, encoding2)
+        partners = self.SUT.waitlist[missing_encoding][0]
+        self.assertIn(encoding1, partners)
+        self.assertIn(encoding2, partners)
+
+        missing_encoding = SetBoard.missing_encoding_from(encoding2, encoding3)
+        partners = self.SUT.waitlist[missing_encoding][0]
+        self.assertIn(encoding2, partners)
+        self.assertIn(encoding3, partners)
+
+        missing_encoding = SetBoard.missing_encoding_from(encoding1, encoding3)
+        partners = self.SUT.waitlist[missing_encoding][0]
+        self.assertIn(encoding1, partners)
+        self.assertIn(encoding3, partners)
+
+    def test_board_find_set_returns_hand_and_moves_cards_into_graveyard(self):
+        encoding1 = self.card1.encoding
+        encoding2 = self.card2.encoding
+        card3 = SetCard(
+            count=3,
+            color=SetColor.purple,
+            shading=SetShading.solid,
+            shape=SetShape.diamond
+        )
+        encoding3 = card3.encoding
+
+        hand = SetHand(self.card1, self.card2, card3)
+        self.assertTrue(hand.is_set())
+
+        self.SUT.cards_by_encoding = {
+            encoding1: self.card1,
+            encoding2: self.card2,
+            encoding3: card3
+        }
+
+        returned_hand = self.SUT.find_set()
+
+        self.assertTrue(returned_hand.is_set())
+
+        self.assertEqual(0, len(self.SUT.cards))
+        self.assertIs(self.card1, self.SUT.graveyard_by_encoding[encoding1])
+        self.assertIs(self.card2, self.SUT.graveyard_by_encoding[encoding2])
+        self.assertIs(card3, self.SUT.graveyard_by_encoding[encoding3])
+
+    def test_board_add_to_waitlist_creates_new_entry_in_waitlist(self):
+        encoding1 = self.card1.encoding
+        encoding2 = self.card2.encoding
+
+        encoding3 = SetBoard.missing_encoding_from(encoding1, encoding2)
+
+        self.assertNotIn(encoding3, self.SUT.waitlist)
+
+        self.SUT.add_to_waitlist(encoding3, encoding1, encoding2)
+
+        partners = self.SUT.waitlist[encoding3][0]
+
+        self.assertIn(encoding1, partners)
+        self.assertIn(encoding2, partners)
+
+    def test_board_find_set_handles_duplicate_waitlist_entries(self):
+        # 0000 = 0 red solid diamond, 1
+        # --------
+        # 0001 = 1 red solid diamond, 2
+        # 0002 = 2 red solid diamond, 3
+        # --------
+        # 0010 = 3 red solid squiggles, 1
+        # 0020 = 6 red solid oval, 1
+        # --------
+        # 0100 = 9 red empty diamond, 1
+        # 0200 = 18 red striped diamond, 1
+
+        missing_card = SetCard(
+            color=SetColor.red,
+            shading=SetShading.solid,
+            shape=SetShape.diamond,
+            count=1
+        )
+        missing_encoding = missing_card.encoding
+        self.assertEqual(0, missing_encoding)
+
+        # -----------------------------------------
+
+        card1 = SetCard(
+            color=SetColor.red,
+            shading=SetShading.solid,
+            shape=SetShape.diamond,
+            count=2
+        )
+        encoding1 = card1.encoding
+        self.assertEqual(1, encoding1)
+
+        card2 = SetCard(
+            color=SetColor.red,
+            shading=SetShading.solid,
+            shape=SetShape.diamond,
+            count=3
+        )
+        encoding2 = card2.encoding
+        self.assertEqual(2, encoding2)
+
+        missing_1_2 = SetBoard.missing_encoding_from(encoding1, encoding2)
+        self.assertEqual(missing_encoding, missing_1_2)
+
+        self.SUT.add_to_waitlist(missing_encoding, encoding1, encoding2)
+
+        # ------------------------------------------
+
+        card3 = SetCard(
+            color=SetColor.red,
+            shading=SetShading.solid,
+            shape=SetShape.squiggles,
+            count=1
+        )
+        encoding3 = card3.encoding
+        self.assertEqual(3, encoding3)
+
+        card4 = SetCard(
+            color=SetColor.red,
+            shading=SetShading.solid,
+            shape=SetShape.oval,
+            count=1
+        )
+        encoding4 = card4.encoding
+        self.assertEqual(6, encoding4)
+
+        missing_3_4 = SetBoard.missing_encoding_from(encoding3, encoding4)
+        self.assertEqual(missing_encoding, missing_3_4)
+
+        self.SUT.add_to_waitlist(missing_encoding, encoding3, encoding4)
+
+        # -------------------------------------------
+
+        card5 = SetCard(
+            color=SetColor.red,
+            shading=SetShading.empty,
+            shape=SetShape.diamond,
+            count=1
+        )
+        encoding5 = card5.encoding
+        self.assertEqual(9, encoding5)
+
+        card6 = SetCard(
+            color=SetColor.red,
+            shading=SetShading.striped,
+            shape=SetShape.diamond,
+            count=1
+        )
+        encoding6 = card6.encoding
+        self.assertEqual(18, encoding6)
+
+        missing_5_6 = SetBoard.missing_encoding_from(encoding5, encoding6)
+        self.assertEqual(missing_encoding, missing_5_6)
+
+        self.SUT.add_to_waitlist(missing_encoding, encoding5, encoding6)
+
+        # ===========================================
+
+        # Check waitlist:
+        partner_sets = self.SUT.waitlist[missing_encoding]
+        self.assertIn(set([encoding1, encoding2]), partner_sets)
+        self.assertIn(set([encoding3, encoding4]), partner_sets)
+        self.assertIn(set([encoding5, encoding6]), partner_sets)
+
+        # Clear waitlist so we can test that this behavior is
+        # preserved through find_set:
+        self.SUT.waitlist = {}
+
+        self.SUT.cards_by_encoding = {
+            encoding1: card1,
+            encoding2: card2,
+            encoding3: card3,
+            encoding4: card4,
+            encoding5: card5,
+            encoding6: card6
+        }
+
+        found_set = self.SUT.find_set()
+
+        self.assertIsNone(found_set)
+
+        # Check waitlist:
+        partner_sets = self.SUT.waitlist[missing_encoding]
+        self.assertIn(set([encoding1, encoding2]), partner_sets)
+        self.assertIn(set([encoding3, encoding4]), partner_sets)
+        self.assertIn(set([encoding5, encoding6]), partner_sets)
